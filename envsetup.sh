57ae7d10d224f76545ced829514eae491a8b9837
iMnX <imnx.au@outlook.com>
2017-09-13 07:09:36 +1000

    added envsetup.sh to aid building for aarch64.

---
+++ envsetup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/sudo /bin/bash
+sudo rm -r kernel-out/
+ARCH=arm64
+CROSS_COMPILE=aarch64-linux-gnu-
+KERNEL=kernel8.img
+INSTALL_FW_PATH=$(pwd)/kernel-out/
+INSTALL_MOD_PATH=$(pwd)/kernel-out/
+INSTALL_HDR_PATH=$(pwd)/kernel-out/usr/
+INSTALL_PATH=$(pwd)/boot/
+mkdir -p kernel-out/boot/ kernel-out/usr/bin/ kernel-out/lib/modules/ kernel-out/lib/firmware/
+export ARCH CROSS_COMPILE KERNEL INSTALL_FW_PATH INSTALL_MOD_PATH INSTALL_PATH
+/bin/bash
